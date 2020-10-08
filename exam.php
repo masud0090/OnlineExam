11c2d0175a74486451dc1216052effd552d5520c
masud0090 <masudrana100855@gmail.com>
2020-10-08 14:02:08 +0600

    first commit

---
+++ exam.php
@@ -0,0 +1,17 @@
+<?php include 'inc/header.php'; ?>
+<?php 
+	Session::checkSession();
+?>
+<div class="main">
+<h1>Welcome to Online Exam - Start Now</h1>
+	<div class="segment" style="margin-right:30px;">
+		<img src="img/download2.jpg"/>
+	</div>
+	<div class="segment">
+	<h2>Start Test From Here</h2>
+	<ul>
+		<li><a href="starttest.php">Start Now...</a></li>
+	</ul>
+	</div>
+	
+  </div>
